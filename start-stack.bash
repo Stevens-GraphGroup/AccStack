fb61aea5a4b62e2ab0ce16e1d400628294f8f052
Dylan Hutchison <dhutchis@stevens.edu>
2015-02-18 23:58:41 -0500

    no accumulo init in start-stack

--- start-stack.bash
+++ start-stack.bash
@@ -3,5 +3,4 @@ $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
 $ZOOKEEPER_HOME/bin/zkServer.sh start
-$ACCUMULO_HOME/bin/accumulo init
 $ACCUMULO_HOME/bin/start-all.sh
